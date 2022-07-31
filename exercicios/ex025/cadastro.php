37b49eff6bc52925aacdd5b3d46de95a379d2fa9
nunogs <bruno_gobo4@hotmail.com>
2022-07-31 15:38:02 -0300

    atualização no php

--- exercicios/ex025/cadastro.php
+++ exercicios/ex025/cadastro.php
@@ -1,1 +1,11 @@
-Os dados forma enviados!
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>arquivo php fake</title>
+</head>
+<body>
+    <h1>Dados recebidos</h1>    
+</body>
+</html>
